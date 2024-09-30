673f78faaadb27ffa26cb27b48bc788e26faec5d
mlcboy <46532854+mlcman@users.noreply.github.com>
2024-09-29 20:48:24 -0500

    Build frontend and fix paths

--- backend/app.py
+++ backend/app.py
@@ -27,9 +27,9 @@ chronotype_peaks = {
 }
 
 # Route to serve the static frontend
-@app.route('/<path:path>')
-def static_serve(path):
-    return send_from_directory(app.static_folder, path)
+@app.route('/')
+def serve():
+    return send_from_directory(app.static_folder, 'index.html')
 
 # API route for optimal gym time
 @app.route('/optimal_gym_time', methods=['GET'])
@@ -129,7 +129,7 @@ def optimal_gym_time():
 @app.errorhandler(404)
 def not_found(e):
     return send_from_directory(app.static_folder, 'index.html')
-
+    
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))  # Get the port from the environment variable or default to 5000
     app.run(host='0.0.0.0', port=port)  # Run the app on all interfaces
